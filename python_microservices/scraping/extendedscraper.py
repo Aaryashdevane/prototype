dc46a69b9ed8cef5706891d2b4dea8cdfb0ffd37
abhishek0948 <abhishekkaratagi325@gmail.com>
2025-04-11 23:39:36 +0530

    Merge conflict resolved

--- python_microservices/scraping/extendedscraper.py
+++ python_microservices/scraping/extendedscraper.py
@@ -77,8 +77,6 @@ async def main_extended():
         await browser.close()
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     asyncio.run(main_extended())
-=======
-    asyncio.run(main_extended())
->>>>>>> main
+
+
